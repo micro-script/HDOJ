4e6b79c5084049f7cdf49684754c0df601997835
micro-script <micro.script@hotmail.com>
2017-06-02 16:59:54 +0800

    Add files via upload

---
+++ HDOJ_2202_VolumeOfBall.cpp
@@ -0,0 +1,19 @@
+/* @CopyRight:   Script by vimer(micro.script@hotmail.com). All rights reserved.
+ * @Version:     1.0
+ * @Date:        Tuesday, May 30, 2017
+ * @Description: Volume of Ball (URL:http://acm.hdu.edu.cn/showproblem.php?pid=2002)
+ * @Environment: Code::Blocks 16.01, GCC 4.9.
+ */
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    double radius = 0.0, volume = 0.0;
+    const double PI = 3.1415927;
+    while(cin >> radius){
+        volume = (4.0 / 3.0) * PI * radius * radius * radius;
+        cout << fixed << setprecision(3) << volume << endl;
+    }
+    return 0;
+}
---
+++ HDOJ_4847_WowSuchDoge.cpp
@@ -0,0 +1,23 @@
+/* @CopyRight:   Script by vimer(micro.script@hotmail.com). All rights reserved.
+ * @Version:     1.0
+ * @Date:        Friday, June 2, 2017
+ * @Description: Wow! Such Doge! (URL: http://acm.hdu.edu.cn/showproblem.php?pid=4847)
+ * @Environment: Code::Blocks 16.01, GCC 4.9.
+ */
+#include<iostream>
+#include<string>
+#include<regex>
+using namespace std;
+int main(){
+    const string REG_EXP_STR = "DOGE";
+    const regex REGEX_EXP(REG_EXP_STR, regex::icase);
+    string reader;
+    int counter = 0;
+    while(getline(cin, reader)){
+        int match_counter = 0;
+        for(sregex_iterator it(reader.begin(), reader.end(), REGEX_EXP), end_it; it != end_it; ++it)++match_counter;
+        counter += match_counter;
+    }
+    cout << counter << endl;
+    return 0;
+}
